000e9e46a8ace3772aa2a7179712876bbfbbb434
mydimons <mydimons.kb@gmail.com>
2024-11-07 17:21:57 -0800

    Variable file added

---
+++ Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        String string = "123";
+        char charecter = 'a';
+        boolean bool = false;
+        int num1 = 1;
+        float num2 = 1.1f;
+        double num3 = 1.1111111112f;
+
+        System.out.println(string + " " + charecter + " " + bool + " " + num1 + " " + num2 + " " + num3);
+    }
+}
--- hashtag_challenge.java
+++ hashtag_challenge.java
@@ -1,15 +1,16 @@
+
 import java.util.Scanner;  // Import the Scanner class
 // OCT 24, 2024
+
 class hashtag_challenge {
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    
-    int height;
-    do{
-        System.out.print("Height: ");
-        height = input.nextInt();
-    }while(height < 0 && height > 8);
-
+      int height;
+      try (Scanner input = new Scanner(System.in)) {
+          do{
+              System.out.print("Height: ");
+              height = input.nextInt();
+          }while(height < 0 && height > 8);
+      }
     for (int i = 1; i <= height; i++) {
         for (int j = height - i; j > 0; j--) {
             System.out.print(" ");
