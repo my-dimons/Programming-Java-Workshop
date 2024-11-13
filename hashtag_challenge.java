cffdb57a9a760628736a7f3b2d5e35f7a0b35a84
mydimons <mydimons.kb@gmail.com>
2024-11-13 14:31:52 -0800

    Update hashtag_challenge.java

--- hashtag_challenge.java
+++ hashtag_challenge.java
@@ -1,24 +1,23 @@
-
-import java.util.Scanner;  // Import the Scanner class
+import java.util.Scanner; // Import the Scanner class
 // OCT 24, 2024
 
 class hashtag_challenge {
-  public static void main(String[] args) {
-      int height;
-      try (Scanner input = new Scanner(System.in)) {
-          do{
-              System.out.print("Height: ");
-              height = input.nextInt();
-          }while(height < 0 && height > 8);
-      }
-    for (int i = 1; i <= height; i++) {
-        for (int j = height - i; j > 0; j--) {
-            System.out.print(" ");
+    public static void main(String[] args) {
+        int height;
+        try (Scanner input = new Scanner(System.in)) {
+            do {
+                System.out.print("Height: ");
+                height = input.nextInt();
+            } while (height < 0 && height > 8);
         }
-        for (int j = 0; j < i; j++) {
-            System.out.print("#");
+        for (int i = 1; i <= height; i++) {
+            for (int j = height - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
         }
-        System.out.println();
     }
-  }
 }
